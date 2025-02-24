a74e253e0d3e42fb7fdb13345d42dfda1c20f7ce
RichieStevanus <richie.stevanus12@gmail.com>
2025-02-24 22:44:57 +0700

    Variable

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	// name = "richie stevanus"
+	// fmt.Println(name)
+
+	// name = "richie new"
+	// fmt.Println(name)
+
+	name := "richie Stevanus"
+	fmt.Println(name)
+
+	name = "richie"
+	fmt.Println(name)
+}
